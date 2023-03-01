325b50e43fa0b63c99b16e2e7dfae91933f30e29
Devum765 <69791840+Devum765@users.noreply.github.com>
2023-03-01 18:51:41 +0900

    Add files via upload

--- 059Scenario010.js
+++ 059Scenario010.js
@@ -475,7 +475,7 @@ function Scenario010() {
 	TaikyakuSystem = 0;
 	MiscShisyu = 1;
 	TikeiCost = new Array(2,99,1,99,2,1,3);//平地、海、都市、湿地、河川、道路、森
-	TikeiBougyo = new Array(0,99,2,99,-1,0,1);
+	TikeiBougyo = new Array(0,99,1,99,-1,0,1);
 	DefMisc = 0;//防衛時の戦力ボーナス
 	MiscTaikyakuSuu = 2;//退却移動するへクス数
 	KyoukouKinou = 1;//強攻
